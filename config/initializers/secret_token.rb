2dbb65d3a0e95358ead0af86bf353e26ca88d6e5
Daniel Pérez Rada <dperezrada@gmail.com>
2014-06-19 15:02:15 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImetricasMailer::Application.config.secret_key_base = '7a3b5a5683865d6c1edea9b826ba2f113d363fb32087288d55ecc053403730f59cdbfd1070ddb17c5e17d67f901e088c3301dc46d7e442ab3c523d29c61ca8b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImetricasMailer::Application.config.session_store :cookie_store, key: '_imetricas_mailer_session'
