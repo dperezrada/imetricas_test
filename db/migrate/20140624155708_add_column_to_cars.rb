983f1244453ebb2e30066424152ead54dfa5e926
Daniel Pérez Rada <dperezrada@gmail.com>
2014-06-24 11:59:14 -0400

    Adding new column to car

---
+++ db/migrate/20140624155708_add_column_to_cars.rb
@@ -0,0 +1,5 @@
+class AddColumnToCars < ActiveRecord::Migration
+  def change
+  	add_column :cars, :year, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621145833) do
+ActiveRecord::Schema.define(version: 20140624155708) do
 
   create_table "cars", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "year"
   end
 
 end
